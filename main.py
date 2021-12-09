655ad3ce142b968da7f8fcb29449eece5fa9f71a
olaf1816 <oalegre@institutmvm.cat>
2021-12-09 13:10:35 +0100

    Ejercicio lista multiples de 2 hasta el 30

--- main.py
+++ main.py
@@ -4,13 +4,14 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    llista = [x for x in range(0, 31, 2)]
+
+    print(llista)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
